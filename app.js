a6781ff2e47bda90c29d89a96f0a184027460db0
Stanley Louis <stanlouis@msn.com>
2018-06-09 09:49:15 -0400

    Getting address with yargs

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
 require('dotenv').config()
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const encodedAddress = encodeURI(argv.address);
 const KEY = process.env.GOOGLE_API_KEY;
 
-var options = {
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?' +
-  'address=1633 yale place rockville md 20850' +
-  '&key=' + KEY,
+const options = {
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${KEY}`,
   json: true
 };
 request(options, (err, response, body) => {
@@ -14,6 +27,4 @@ request(options, (err, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  
-  
 });
