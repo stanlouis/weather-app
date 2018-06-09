d86f64fbf1d2a359ec0ebe26361c3e4137bfecd4
Stanley Louis <stanlouis@msn.com>
2018-06-09 14:12:21 -0400

    Getting the weather info from dark_sky_api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const yargs = require('yargs');
 require('dotenv').config()
 const geocode = require('./geocode/geocode');
-
+const weather = require('./weather/weather');
 const argv = yargs
   .options({
     a: {
@@ -16,17 +16,25 @@ const argv = yargs
   .argv;
 
 const encodedAddress = encodeURI(argv.address);
-const KEY = process.env.GOOGLE_API_KEY;
+const google_api_key = process.env.GOOGLE_API_KEY;
+const dark_sky_api_key = process.env.DARK_SKY_API_KEY;
 
 const options = {
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${KEY}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${google_api_key}`,
   json: true
 };
 
 geocode.geocodeAddress(options, (errorMessage, results) => {
-  if(errorMessage) {
+  if (errorMessage) {
     console.log(errorMessage)
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, dark_sky_api_key, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
   }
 });
