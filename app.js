457121e2bbb5f74ab966aa7bfd6dfb376125e2c4
Stanley Louis <stanlouis@msn.com>
2018-06-09 08:32:14 -0400

    pretty-print-json

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1633+yale+place+rockville+md+20850',
   json: true
 }, (err, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
