5e55b6aeb1ce20cda080dd00045884dfeb20f444
Stanley Louis <stanlouis@msn.com>
2018-06-09 09:27:30 -0400

    Use dotenv for api key

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const request = require('request');
+require('dotenv').config()
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1633+yale+place+rockville+md+20850',
+const KEY = process.env.GOOGLE_API_KEY;
+
+var options = {
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?' +
+  'address=1633 yale place rockville md 20850' +
+  '&key=' + KEY,
   json: true
-}, (err, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+};
+request(options, (err, response, body) => {
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  
+  
 });
